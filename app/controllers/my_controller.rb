76eb3f5cee4fcb769a94cc0dd0051ea50a69f486
Jerome Borenstejn <jeromeborenstejn@gmail.com>
2023-02-13 14:18:46 +0100

    wip

--- app/controllers/my_controller.rb
+++ app/controllers/my_controller.rb
@@ -1,22 +1,4 @@
 class MyController < ApplicationController
-  def show
-    respond_to do |format|
-      format.html
-      format.pdf do
-        # Rails 6:
-        # render template: "posts/show.html.erb",
-        #        pdf: "Post ID: #{@post.id}"
-
-        # Rails 7:
-        render pdf: [2, "@post.name"].join('-'),
-               template: "posts/show.html.erb",
-               formats: [:html],
-               disposition: :inline,
-               layout: 'pdf'
-      end
-    end
-  end
-
   def index
     respond_to do |format|
       format.html
@@ -34,16 +16,5 @@ class MyController < ApplicationController
                layout: 'pdf'
       end
     end
-  end
-
-  private
-
-  def get_html
-    ActionController::Base.new.render_to_string(
-      template: 'v1/profile/reportes.html.erb',
-      orientation: 'Landscape',
-      page_size: 'Letter',
-      background: true
-    )
   end
 end
